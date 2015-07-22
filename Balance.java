367709ac539bd22fa6b3fbb6801c29076310bc74
jessica <ladyjtm@gmail.com>
2015-07-22 10:38:57 -0700

    Add java balance

---
+++ Balance.java
@@ -0,0 +1,17 @@
+public class Balance
+
+{
+  public static void main(String[] args)
+
+  {
+     int init_bal = 1000;
+     // int year_two_bal = init_bal * 1.05;
+     // int year_three_bal = year_two_bal * 1.05;
+
+     System.out.println("The first year balance is $" + Integer.toString(init_bal) + ".");
+     // System.out.println("The second year balance is $" + Integer.toString(year_two_bal) + ".");
+     // System.out.println("The third year balance is $" + Integer.toString(year_three_bal) + ".");
+
+  }
+
+}
